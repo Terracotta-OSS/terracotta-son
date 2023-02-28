fbec45813626a61307781b50d63e86166f48a67f
Tom Mesic <59479956+tmesic99@users.noreply.github.com>
2023-02-28 17:02:02 -0500

    Upgrade to ph-javacc-maven-plugin 4.1.5

--- src/test/java/com/terracottatech/tcson/ReadableSonTest.java
+++ src/test/java/com/terracottatech/tcson/ReadableSonTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011-2018 Software AG, Darmstadt, Germany and/or Software AG USA Inc., Reston, VA, USA, and/or its subsidiaries and/or its affiliates and/or their licensors.
+ * Copyright (c) 2011-2023 Software AG, Darmstadt, Germany and/or Software AG USA Inc., Reston, VA, USA, and/or its subsidiaries and/or its affiliates and/or their licensors.
  * Use, reproduction, transfer, publication or disclosure is prohibited except as specifically provided for in your License Agreement with Software AG.
  */
 package com.terracottatech.tcson;
@@ -60,6 +60,7 @@ public class ReadableSonTest {
       .append(32.5f)
       .endList();
     writer.append("barr", (byte) 1, new byte[] { 1, 2, 4, 8 }, 0, 4);
+    writer.append("barr-none", (byte) 1, new byte[0], 0, 0);
     writer.endMap();
 
     ByteBuffer buf = writer.buffer().getBuffer();
@@ -71,6 +72,8 @@ public class ReadableSonTest {
     Assert.assertThat(m.get("fl").floatValue(), is(32.5f));
     Assert.assertThat(m.get("barr").bytesValue().getSignifier(), is((byte) 1));
     Assert.assertThat(m.get("barr").bytesValue().getBuffer(), is(ByteBuffer.wrap(new byte[] { 1, 2, 4, 8 })));
+    Assert.assertThat(m.get("barr-none").bytesValue().getSignifier(), is((byte) 1));
+    Assert.assertThat(m.get("barr-none").bytesValue().getBuffer(), is(ByteBuffer.wrap(new byte[0])));
     ReadableSonList l = m.get("lst").listValue();
     Assert.assertThat(l.get(0).intValue(), is(10));
     Assert.assertThat(l.get(1).intValue(), is(11));
@@ -106,7 +109,9 @@ public class ReadableSonTest {
       .endMap()
       .append(32.5f)
       .endList();
+    writer.append("barr-none", (byte) 1, new byte[0], 0, 0);
     writer.append("barr", (byte) 1, new byte[] { 1, 2, 4, 8 }, 0, 4);
+    writer.append("barr-zero", (byte) 0, new byte[0], 0, 0);
     writer.endMap();
 
     ByteBuffer buf = writer.buffer().getBuffer();
@@ -118,6 +123,10 @@ public class ReadableSonTest {
     Assert.assertThat(m.get("fl").floatValue(), is(32.5f));
     Assert.assertThat(m.get("barr").bytesValue().getSignifier(), is((byte) 1));
     Assert.assertThat(m.get("barr").bytesValue().getBuffer(), is(ByteBuffer.wrap(new byte[] { 1, 2, 4, 8 })));
+    Assert.assertThat(m.get("barr-none").bytesValue().getSignifier(), is((byte) 1));
+    Assert.assertThat(m.get("barr-none").bytesValue().getBuffer(), is(ByteBuffer.wrap(new byte[0])));
+    Assert.assertThat(m.get("barr-zero").bytesValue().getSignifier(), is((byte) 0));
+    Assert.assertThat(m.get("barr-zero").bytesValue().getBuffer(), is(ByteBuffer.wrap(new byte[0])));
     SonList<?> l = m.get("lst").listValue();
     Assert.assertThat(l.get(0).intValue(), is(10));
     Assert.assertThat(l.get(1).intValue(), is(11));
@@ -194,6 +203,8 @@ public class ReadableSonTest {
     writer.append(dt);
     writer.appendNull();
     writer.append(uuid);
+    writer.append((byte) 0, new byte[0]);
+    writer.append((byte) 10, new byte[0]);
     writer.endList();
 
     ByteBuffer buf = writer.buffer().getBuffer();
@@ -214,6 +225,8 @@ public class ReadableSonTest {
     Assert.assertThat(m.get(9).dateValue(), is(dt));
     Assert.assertThat(m.get(10).isNullValue(), is(true));
     Assert.assertThat(m.get(11).uuidValue(), is(uuid));
+    Assert.assertThat(m.get(12).bytesValue().getBuffer(), is(ByteBuffer.wrap(new byte[0])));
+    Assert.assertThat(m.get(13).bytesValue().getBuffer(), is(ByteBuffer.wrap(new byte[0])));
   }
 
   @Test
@@ -232,6 +245,7 @@ public class ReadableSonTest {
     writer.append("key7hh", '&');
     writer.append("key84", "some string");
     writer.append("key9gg", (byte) 10, barr);
+    writer.append("key9hh", (byte) 10, new byte[0]);
     writer.append("key10o", dt);
     writer.appendNull("kelly11");
     writer.append("kerry12", uuid);
@@ -250,6 +264,8 @@ public class ReadableSonTest {
     Assert.assertThat(m.get("keloy4").longValue(), is(12L));
     Assert.assertThat(m.get("key9gg").bytesValue().getSignifier(), is((byte) 10));
     Assert.assertThat(m.get("key9gg").bytesValue().getBuffer(), is(ByteBuffer.wrap(barr)));
+    Assert.assertThat(m.get("key9hh").bytesValue().getSignifier(), is((byte) 10));
+    Assert.assertThat(m.get("key9hh").bytesValue().getBuffer(), is(ByteBuffer.wrap(new byte[0])));
     Assert.assertThat(m.get("kedsfy5").floatValue(), is(13.0f));
     Assert.assertThat(m.get("key84").stringValue(), is("some string"));
     Assert.assertThat(m.get("keddy6").doubleValue(), is(13.0d));
