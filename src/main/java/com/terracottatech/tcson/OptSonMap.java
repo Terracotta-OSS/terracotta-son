3833f6a66178746b4a63e510c7be90fff64e8fab
Clifford W. Johnson <clifford.johnson@softwareag.com>
2025-01-08 15:21:15 -0500

    Transition from Super iPaas Integration LLC to IBM

--- src/main/java/com/terracottatech/tcson/OptSonMap.java
+++ src/main/java/com/terracottatech/tcson/OptSonMap.java
@@ -1,5 +1,5 @@
 /*
- * Copyright Super iPaaS Integration LLC, an IBM Company 2020, 2024
+ * Copyright IBM Corp. 2020, 2025
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
